0fc53764931b2e0841696a22717c64552324ba16
Othman Tenich <otenich@gmail.com>
2020-11-14 19:17:49 +0100

    Added current time indicator and bar to help identify how many time is remaning in the current slot and what starts next

--- src/views/Schedule2.vue
+++ src/views/Schedule2.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="schedule"
        :style="{width: combinedSchedule.length * width + 10 + 'px'}">
+    <div class="now-bar" :style="{top: nowBarOffset + 'px'}">
+      <div class="now-indicator"></div>
+    </div>
     <div
       class="wrapper"
       :style="{height: containerHeight + 'px', width: combinedSchedule.length * width + 10 + 'px'}"
@@ -32,8 +35,14 @@ export default Vue.extend({
   data() {
     return {
       ratio: 2.6,
-      width: 195
+      width: 195,
+      nowBarOffset: 0
     };
+  },
+  created() {
+    const v = this as any;
+    setInterval(v.updateNowBarOffset, 5 * 60 * 1000);
+    v.updateNowBarOffset();
   },
   computed: {
     combinedSchedule() {
@@ -53,6 +62,23 @@ export default Vue.extend({
         top: item.top * this.ratio + 10 + 'px',
         left: this.width * index + 10 + 'px'
       };
+    },
+    updateNowBarOffset() {
+      const v = this as any;
+      const firstTalk = v.combinedSchedule.flatMap((array: UiScheduleEvent[][]) => array)
+        .sort((a: UiScheduleEvent, b: UiScheduleEvent) => moment(a.toTime).diff(moment(b.toTime)))[0];
+      const now = moment(new Date().toLocaleTimeString(), 'HH:mm');
+      const startOfDay = moment(new Date(firstTalk.fromTime).toLocaleTimeString(), 'HH:mm');
+      const result = now.diff(startOfDay, 'minute') * v.ratio;
+      if (result < 0) {
+        v.nowBarOffset = 0;
+      }
+      else if (result > v.containerHeight) {
+        v.nowBarOffset = v.containerHeight;
+      }
+      else {
+        v.nowBarOffset = result;
+      }
     }
   }
 });
@@ -87,5 +113,24 @@ export default Vue.extend({
   &.keynote {
     width: calc(100% - 20px);
   }
+}
+
+.now-bar {
+  position: absolute;
+  width: 100%;
+  height: 3px;
+  background-color: black;
+  z-index: 2;
+}
+
+.now-indicator {
+  display : inline-block;
+  height : 0;
+  width : 0;
+  border-top : 4px solid transparent;
+  border-bottom : 6px solid transparent;
+  border-left : 10px solid black;
+  position: absolute;
+  top: -3px;
 }
 </style>
